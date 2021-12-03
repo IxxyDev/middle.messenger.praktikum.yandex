5b57867f26dab00a3eaf19c8af5dedf98676bbde
user <user@MacBook-Pro-user.local>
2021-12-04 00:50:09 +0300

    fix: fix styles

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./dist/pages'));
+app.use(express.static('./dist/'));
 
 app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}!`);
 });
