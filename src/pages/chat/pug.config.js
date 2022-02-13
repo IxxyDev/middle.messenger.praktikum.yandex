775420c68d91e593945d4340b4467db76c6d0bc9
IxxyDev <denisenkoforwork@gmail.com>
2022-02-13 21:24:13 +0300

    feat: sidebar ready

---
+++ src/pages/chat/pug.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  locals: {
+    createArray: function(count) {
+      return new Array(count).fill('')
+    },
+    messages: [
+      {
+        message: 'Lorem ipsum',
+        time: '12:00'
+      },
+      {
+        message: 'Lorem ipsum',
+        time: '12:00'
+      },
+      {
+        message: 'Lorem ipsum',
+        time: '12:00'
+      },
+      {
+        message: 'Lorem ipsum',
+        time: '12:00'
+      },
+      {
+        message: 'Lorem ipsum',
+        time: '12:00'
+      },
+      {
+        message: 'Lorem ipsum',
+        time: '12:00'
+      },
+    ]
+  }
+}
