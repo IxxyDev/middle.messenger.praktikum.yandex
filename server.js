3a337ad5d2889fcf0790ddabc7f97a4f90c01c6c
IxxyDev <denisenkoforwork@gmail.com>
2022-03-28 21:58:23 +0300

    feat: add congifs and install packages

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./dist/'));
+app.use(express.static(path.join(__dirname, '../dist'));
+app.get('/*', (req, res) => {
+  res.sendFile('index.html') , { root: path.join(__dirname, '../dist')}
+})
 
 app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}!`);
