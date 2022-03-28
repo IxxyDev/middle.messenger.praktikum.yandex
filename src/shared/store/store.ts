d749f7fcb6617b7c0aefcbdbf90945ecf0f14298
IxxyDev <denisenkoforwork@gmail.com>
2022-03-28 23:10:37 +0300

    feat: add general block element

---
+++ src/shared/store/store.ts
@@ -0,0 +1,20 @@
+import {EventBus} from '../EventBus'
+import {State} from "../global";
+import {INITIAL_STATE} from "./initialState/initialState";
+
+
+class Store extends EventBus {
+  private state: State = INITIAL_STATE;
+
+  public getState(): State {
+    return this.state;
+  }
+
+  public setState(path: string, value: unknown, eventName: string): void {
+    set(this.state, path, value);
+
+    this.emit(eventName, path);
+  };
+}
+
+export default new Store();
