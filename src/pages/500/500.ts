dd3315679dbb4f13cc5802c8acf348188f02ccf1
IxxyDev <denisenkoforwork@gmail.com>
2022-03-29 18:35:31 +0300

    feat: add 500 page

--- src/pages/500/500.ts
+++ src/pages/500/500.ts
@@ -3,11 +3,11 @@ import {ElementEvents} from "../../shared/global"
 import {compile} from '../../shared/utils/compileTemplate'
 import {router} from '../../routes'
 
-import templatePug from './404.pug'
-import '404.scss'
-import {Page404Props} from "./interfaces"
+import templatePug from './500.pug'
+import '500.scss'
+import {Page500Props} from "./interfaces"
 
-const page404Events: ElementEvents = {
+const page500Events: ElementEvents = {
   click: [
     {
       id: 'toChats',
@@ -19,8 +19,8 @@ const page404Events: ElementEvents = {
   ]
 }
 
-export class Page404 extends Block<Page404Props> {
-  constructor(props: Page404Props, events: ElementEvents = page404Events, rootId?: string) {
+export class Page500 extends Block<Page500Props> {
+  constructor(props: Page500Props, events: ElementEvents = page500Events, rootId?: string) {
     super('div', 'error', props, events, rootId);
   }
 
