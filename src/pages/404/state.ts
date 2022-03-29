6c3c066a1bccaac26e71cfc15dd77f05d9c2cc64
IxxyDev <denisenkoforwork@gmail.com>
2022-03-29 18:40:13 +0300

    feat: add states from 404 and 500 pages

---
+++ src/pages/404/state.ts
@@ -0,0 +1,7 @@
+import {Page404Props} from '../404/interfaces'
+
+export const page404State: Page404Props = {
+  title: '404',
+  text: 'Oops, seems you were knock-knock-knockin\' on wrong door',
+  linkText: 'Back to chats'
+}
---
+++ src/pages/500/state.ts
@@ -0,0 +1,7 @@
+import {Page500Props} from '../500/interfaces'
+
+export const page500State: Page500Props = {
+  title: '500',
+  text: 'Something went completely wrong',
+  linkText: 'Back to chats'
+}
