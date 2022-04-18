0872d38693f97e1801623df5908439616dface54
IxxyDev <denisenkoforwork@gmail.com>
2022-04-18 16:00:35 +0300

    feat: add search input block

--- src/components/Message/interfaces.ts
+++ src/components/Message/interfaces.ts
@@ -1,5 +1,6 @@
 import {Props} from "../../shared/global";
 import {AvatarProps} from "../Avatar/interfaces";
+import {TimeProps} from "../Time/interfaces";
 
 export interface MessageProps extends Props {
   you: boolean
