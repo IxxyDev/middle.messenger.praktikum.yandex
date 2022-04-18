00209e793d5195ec715f38f33f4e3280047112ef
IxxyDev <denisenkoforwork@gmail.com>
2022-04-18 16:00:35 +0300

    feat: add search input block

---
+++ src/components/SearchPanel/SearchPanel.ts
@@ -0,0 +1,20 @@
+import {Block} from "../../shared/Block/Block";
+import {mapStateToProps} from "../../shared/store/mapStateToProps";
+import {ElementEvents} from "../../shared/global";
+import {compile} from "../../shared/utils/compileTemplate";
+
+import template from './SearchPanel.pug';
+import './SearchPanel.scss';
+import {SearchPanelProps} from "./interfaces";
+
+export class SearchPanel extends Block<SearchPanelProps> {
+  constructor(props: SearchPanelProps, event: string, events?: ElementEvents) {
+    super('div', 'search-panel', props, events);
+
+    this.subscribe(event, mapStateToProps)
+  }
+
+  render() {
+    return compile(template, this.props, '', this.meta.events)
+  }
+}
