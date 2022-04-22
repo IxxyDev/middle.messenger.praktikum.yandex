da1d4991b2b6cda38dca19a46a09343dc44d13d3
IxxyDev <denisenkoforwork@gmail.com>
2022-04-22 13:06:50 +0300

    feat: add initial state for 404 and 500

---
+++ src/shared/store/initialState.ts
@@ -0,0 +1,7 @@
+import {page404State} from "../../pages/404/state";
+import {page500State} from "../../pages/500/state";
+
+export const initialState = {
+  page404: page404State,
+  page500: page500State,
+}
