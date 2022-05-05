fe9b9e8d0a349e4cc1c911adc5bfa04eab14f19e
IxxyDev <denisenkoforwork@gmail.com>
2022-05-05 15:31:21 +0300

    feat: logic ready

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,18 @@
 import { Router } from "./shared/Router/Router"
 import { Page404 } from "./pages/404/404"
 import { Page500 } from "./pages/500/500"
+import { ChatPage } from "./pages/chat/chatPage";
+import { SettingsPage } from "./pages/settings/settings";
+import { SignIn } from "./pages/signin/signin";
+import { SignUp } from "./pages/signup/signup";
 
 export const router = new Router("root")
 
 router.use("/404", Page404)
   .use("/500", Page500)
   .use("/chats", ChatPage)
+  .use("/chats", SettingsPage)
+  .use("/chats", SignIn)
+  .use("/chats", SignUp)
   .fallback("/404", Page404)
   .start()
