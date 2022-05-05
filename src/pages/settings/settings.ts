56f21a5e89dacb819a051347b8cc30643ee03558
IxxyDev <denisenkoforwork@gmail.com>
2022-05-05 14:48:13 +0300

    feat: add service for settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -2,14 +2,20 @@ import { ElementEvents } from "../../shared/global";
 import { Block } from "../../shared/Block/Block";
 import { mapStateToProps } from "../../shared/store/mapStateToProps";
 import { compile } from "../../shared/utils/compileTemplate";
+import { SettingsPageProps } from "./interfaces";
+import { GetUserInfoController } from "../../controllers/profile/getUserInfo";
+import { settingsState } from "./state";
+
+import template from './settings.pug'
+import { SETTINGS_EVENT, UPDATE_USER_EVENT } from "./events";
 
 export class SettingsPage extends Block<SettingsPageProps> {
-  constructor(props: SettingsPageProps = settingsPageState, events: ElementEvents, rootId?: string ) {
+  constructor(props: SettingsPageProps = settingsState, events: ElementEvents, rootId?: string ) {
     super('main', 'settingsPage', props, events, rootId);
 
     this.subscribe(SETTINGS_EVENT, mapStateToProps)
     this.subscribe(UPDATE_USER_EVENT, mapStateToProps)
-    UserInfoController.getUserInfo()
+    GetUserInfoController.getInfo()
   }
 
   componentDidMount() {
